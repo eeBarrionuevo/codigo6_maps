36608edb72dac546aae23a7b5a20e6dc91e82487
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-04-17 19:58:16 -0500

    Mods

--- lib/pages/permission_page.dart
+++ lib/pages/permission_page.dart
@@ -1,6 +1,29 @@
+import 'package:codigo6_maps/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class PermissionPage extends StatelessWidget {
+class PermissionPage extends StatefulWidget {
+  @override
+  State<PermissionPage> createState() => _PermissionPageState();
+}
+
+class _PermissionPageState extends State<PermissionPage> {
+  checkPermission(PermissionStatus status) {
+    switch (status) {
+      case PermissionStatus.granted:
+        //Ir a la página de inicio
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomePage()));
+        break;
+      case PermissionStatus.denied:
+      case PermissionStatus.limited:
+      case PermissionStatus.permanentlyDenied:
+      case PermissionStatus.restricted:
+        openAppSettings();
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +45,11 @@ class PermissionPage extends StatelessWidget {
               height: 12.0,
             ),
             OutlinedButton(
-              onPressed: () {},
+              onPressed: () async {
+                // print(await Permission.location.status);
+                PermissionStatus status = await Permission.location.request();
+                checkPermission(status);
+              },
               child: const Text(
                 "Activar GPS",
               ),
